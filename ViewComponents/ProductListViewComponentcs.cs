ec253cdc6435f1a696fba1107d078637c792ceb7
mikaylarandall <mikayla.mae@icloud.com>
2024-04-10 14:35:56 -0600

    ProductListViewComponent created

---
+++ ViewComponents/ProductListViewComponentcs.cs
@@ -0,0 +1,10 @@
+﻿namespace IntexBrickwell.ViewComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+    using IntexBrickwell.Models;
+    using IntexBrickwell.Utilities;
+    public class ProductListViewComponentcs : ViewComponent
+    {
+
+    }
+}
