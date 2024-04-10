5cd0c534bb1d9a1670f09a5748d580ed687a2018
blinds25 <120234661+blinds25@users.noreply.github.com>
2024-04-09 18:47:05 -0600

    scaffold attempt

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntexBrickwell.Models;
+
+public partial class User
+{
+    public string? Username { get; set; }
+
+    public int? UserId { get; set; }
+}
